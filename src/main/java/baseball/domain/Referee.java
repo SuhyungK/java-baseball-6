4dd80c5f34b76352eba0305edadfe8f5923f2f5c
SuhyungK <dook4966@gmail.com>
2023-11-07 14:15:45 +0900

    feat(Referee) : 컴퓨터의 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -4,6 +4,21 @@ import java.util.List;
 
 public class Referee {
     public String compare(List<Integer> computer, List<Integer> player) {
-        return "3 스트라이크";
+        // 몇 개의 숫자가 같은 지 알아낸 뒤
+        // 스트라이크의 개수를 구해서 뺀다.
+        // 남은 수는 볼의 개수이다.
+
+        final Judgement judgement = new Judgement(); // 객체들이 서로 협력해서 하나의 프로그램을 완성한다...
+        int correctCount = judgement.correctCount(computer, player);
+
+        int strike = 0;
+        for (int placeIndex = 0; placeIndex < computer.size(); placeIndex++) {
+            if (judgement.hasPlace(computer, placeIndex, player.get(placeIndex))) {
+                strike++;
+            }
+        }
+
+        int ball = correctCount - strike;
+        return ball + " 볼 " + strike + " 스트라이크";
     }
 }

350f4cc6340825a98edef09368c2f02157b3c465
SuhyungK <dook4966@gmail.com>
2023-11-07 12:56:23 +0900

    feat(Judgement) : 몇 개의 숫자가 같은지 알 수 있는 기능 구현

--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 public class Judgement {
     public int correctCount(List<Integer> computer, List<Integer> player) {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            int playerNumber = player.get(i);
+            if (computer.contains(playerNumber)) {
+                result++;
+            }
+        }
+        return result;
     }
 
-    public boolean hasPlace(int placeNumber, int number     ) {
-        // TODO: 특정 자리에 특정 숫자가 있는지  
+    public boolean hasPlace(List<Integer> computer, int placeNumber, int number) {
+        // TODO: 특정 자리에 특정 숫자가 있는지
         return false;
     }
 }
